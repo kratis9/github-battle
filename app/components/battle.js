b87c19619357ca186ab3201201cfc5643bf608b9
Krati Chauhan <kratis9@gmail.com>
2020-07-23 12:43:44 +1000

    Bunded queue, avoid underflow, overflow

--- app/components/battle.js
+++ app/components/battle.js
@@ -27,6 +27,29 @@ function Instructions() {
   );
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      username:''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+
+
+    this.props.onSubmit
+  }
+
+  render() {
+    return (
+      <form className='column-player' onSubmit={this.handleSubmit}></form>
+    )
+  }
+}
+
 export default class Battle extends React.Component {
   render() {
     return (
